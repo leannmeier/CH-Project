de80f6a5f064e55bf4058ad9e0f3f7a0294c035f
Leandro Agustin Meier <leannmeier72@gmail.com>
2026-03-01 14:15:30 -0300

    fix: correción de errores - validación de nombre

--- scripts/main.js
+++ scripts/main.js
@@ -1,40 +1,78 @@
-// Simulador de tienda iPRO - Trabajo Integrador -  Pre-Entrega 1
+// Simulador de tienda iPRO - Trabajo Integrador - Entrega 2
 // Autor: Meier Leandro Agustín ©
-
-console.log('Sincronización exitosa')
 
 const DIAS_DESCUENTO = ['martes','jueves'] // Dias que serán los de descuento
 const PRODUCTOS = [
-    {nombre: 'MacBook 14 Pro Ultimate', precio: 7000000, descuento: 0.90},
-    {nombre: 'iPhone 17 PRO MAX', precio: 3000000, descuento: 0.92},
-    {nombre: 'iPhone Air', precio: 2500000, descuento: 0.88},
-    {nombre: 'AirPods Pro', precio: 250000, descuento: 0.85}
+    {
+        id: 1,
+        nombre: 'MacBook 14 Pro Ultimate', 
+        precio: 7000000, 
+        descuento: 0.90
+    },
+    {
+        id: 2,
+        nombre: 'iPhone 17 PRO MAX', 
+        precio: 3000000, 
+        descuento: 0.92
+    },
+    {
+        id: 3,
+        nombre: 'iPhone Air', 
+        precio: 2500000, 
+        descuento: 0.88
+    },
+    {
+        id: 4,
+        nombre: 'AirPods Pro',
+        precio: 250000, 
+        descuento: 0.85
+    },
 ] // Los productos disponibles
 
-const nombre = prompt('¡Bienvenido a iPRO! ¿Cuál es tu nombre?')
+const nombre = validarNombre()
 
-saludar(nombre)
-
-if(diaDescuento() && nombre !== null){
-    alert('¡APROVECHA QUE HOY ES DÍA DE DESCUENTOS!🎉🥳🎊');
-} else if(nombre === null) {
-    alert('No hay descuentos para vos, bigote')
-} else{
-    alert('Hoy no es dia de descuentos')
+if(nombre){
+    alert(`Hola ${nombre}! Un gusto que estes acá\nDisfruta de los productos y compra algo!`)
+    ofrecerProductos()
+}
+else{
+    alert('Hubo un error en sus datos. No podrá comprar pero si mirar los precios! ')
 }
 
-if(nombre !== null){
-    let seguirComprando = true
-    while(seguirComprando){
-        const productoElegido = prompt('¿Qué producto te interesa?\n1. MacBook\n2. iPhone 17 Pro Max\n3. iPhone Air\n4. AirPods Pro');
-        const producto = obtenerProducto(parseInt(productoElegido)) 
-        if(producto !== null){
+
+function validarNombre(){ // Metodo para validar el nombre de entrada utilizando regex
+    let nombre
+    do{
+        nombre = prompt('¡Bienvenido a iPRO! ¿Cuál es tu nombre?');
+        if (nombre === null) break; // Si el usuario cancela
+        if (!/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/.test(nombre.trim())) {
+            alert('Por favor ingresá solo letras, sin números ni símbolos.');
+        }
+    }while (!/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/.test(nombre.trim()));
+    return nombre
+}
+
+function ofrecerProductos(){
+    let salir = false
+    while(!salir){
+        const productoElegido = prompt('¿Qué producto te interesa?\n1. MacBook\n2. iPhone 17 Pro Max\n3. iPhone Air\n4. AirPods Pro\n0. Salir');
+        console.log(productoElegido)
+        if(productoElegido < PRODUCTOS.length + 1 && productoElegido > 0){
+            const producto = PRODUCTOS.find(obj => obj.id === parseInt(productoElegido)) 
+            console.log(producto)
             const precioFinal = calcularPrecioFinal(producto,diaDescuento())
             alert(`${producto.nombre}\nPrecio sin descuento: ${producto.precio}\nPrecio final: $${precioFinal.toLocaleString()}`);
-        } else{
+        }
+        else if(parseInt(productoElegido) === 0){
+            alert('Usted esta por salir');
+        }
+        else if(productoElegido === null){
+            alert('No ha seleccionado nada');
+        }
+        else{
             alert('Producto inexistente');
         }
-        const continuar = confirm('¿Queres ver otro producto?')
+        const continuar = confirm('¿Desea continuar?')
         if(!continuar){
             seguirComprando = !seguirComprando
             alert('Gracias por venir a iPRO!!!')
@@ -42,16 +80,7 @@ if(nombre !== null){
     }
 }
 
-function saludar(nombre){ //Método de bienvenida
-    if(nombre !== null){
-    alert(`Hola ${nombre}! Un gusto que estes acá\nDisfruta de los productos y compra algo!`)
-    }
-    else{
-        alert('Oh sos re ortiva chabón...')
-    }
-}
-
-function diaDescuento() { // Método que determina si el dia de hoy es es dis de descuento
+function diaDescuento() { // Método que determina si el dia de hoy es día de descuento
     let descuento = false
     let i = 0
     const hoy = new Date()
@@ -64,11 +93,6 @@ function diaDescuento() { // Método que determina si el dia de hoy es es dis de
         i++
     }
     return descuento
-}
-
-function obtenerProducto(index){ // Método que devuelve el producto seleccionado. Si no lo encuentra, devuelve null
-    const productoEncontrado = PRODUCTOS.at(index -1)
-    return productoEncontrado !== undefined ? productoEncontrado : null
 }
 
 function calcularPrecioFinal(producto,estado){ // Método que devuelve el valor final del producto con el descuento incluido
